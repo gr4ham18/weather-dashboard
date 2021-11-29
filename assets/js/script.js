e5a5c342b2a30d1ffd7d34aadb293a8917cdc330
graham <gr4ham18@gmail.com>
2021-11-28 22:08:04 -0500

    search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 var apikey = "008f218670e357c6ca33986604e1c5bb";
 //Set the units to Farenheit
 var units = "imperial"; 
+
+  //JQuery Event click Search Button
+  $("#searchBtn").on("click", function () {
+    var cityValue = $("#search").val();
+    $("#search").val("");
+    weatherForcast(cityValue);
+  });
