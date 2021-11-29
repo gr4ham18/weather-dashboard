c557b698a18ebdba77fd0565a3107908bd9d0768
graham <gr4ham18@gmail.com>
2021-11-28 22:09:51 -0500

    create function with cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,33 @@ $(document).ready(function () {
       weatherForcast(historyValue);
     });
   
+    // Define function to create row with previous cities
+    function createRow(city) {
+      var historyListItem = $("#history-list");
+      var li = $("<li>")
+        .addClass("list-group-item list-group-item-action")
+        .text(city);
+      historyListItem.prepend(li);
+    }
+  
+    function weatherForcast(cityValue) {
+      var queryURL =
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        cityValue +
+        "&units=" +
+        units +
+        "&appid=" +
+        apikey;
+  
+      $.ajax({
+        url: queryURL,
+        type: "GET",
+        dataType: "json",
+      }).then(function (response) {
+        if (history.indexOf(cityValue) === -1) {
+          history.push(cityValue);
+          window.localStorage.setItem("history", JSON.stringify(history));
+          createRow(cityValue);
+        }
+  
+        $("#today").empty();
