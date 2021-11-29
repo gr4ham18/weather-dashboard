d1e83428e60848ce9cf1a34f50fd2d8f5234975b
graham <gr4ham18@gmail.com>
2021-11-28 22:08:52 -0500

    set jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,19 @@
-var apikey = "008f218670e357c6ca33986604e1c5bb";
-//Set the units to Farenheit
-var units = "imperial"; 
+$(document).ready(function () {
 
-  //JQuery Event click Search Button
-  $("#searchBtn").on("click", function () {
-    var cityValue = $("#search").val();
-    $("#search").val("");
-    weatherForcast(cityValue);
-  });
+    var apikey = "008f218670e357c6ca33986604e1c5bb";
+    //Set the units to Farenheit
+    var units = "imperial"; 
+  
+    //JQuery Event click Search Button
+    $("#searchBtn").on("click", function () {
+      var cityValue = $("#search").val();
+      $("#search").val("");
+      weatherForcast(cityValue);
+    });
+  
+    // Set JQuery
+    $("#history-list").on("click", "li", function () {
+      var historyValue = $(this).text();
+      weatherForcast(historyValue);
+    });
+  
